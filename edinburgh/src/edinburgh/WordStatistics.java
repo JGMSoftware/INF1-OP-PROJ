9bb3fa94b2c4f0b63fcec3536b42c702bc119f85
Giedriuszeb <Giedriuszeb@gmail.com>
2013-02-07 16:32:18 -0800

    Create WordStatistics.java

---
+++ edinburgh/src/edinburgh/WordStatistics.java
@@ -0,0 +1,35 @@
+public class WordStatistics {
+Word head=new Word();
+int totalWords=0;
+
+public WordStatistics(){
+head=new Word ();
+totalWords=0;
+}
+
+public void addWord (String newWord, Word current){
+if (current.word==null) {
+  current.set(newWord);
+  current.next=new Word();
+  }
+ 
+if (newWord.equals(current.word)) {current.count++;}
+else {addWord(newWord, current.next);}
+
+totalWords++;
+}
+
+public void printStats(){
+if (head.word!=null){printWordStatsRec(head);}
+}
+
+public void printWordStatsRec(Word toPrint){
+printWordStats(toPrint);
+if (toPrint.next.word!=null) printWordStatsRec(toPrint.next);
+}
+
+public void printWordStats(Word toPrint){
+System.out.println("\""+toPrint.word+"\"" + " was used "+toPrint.count+ " times");
+}
+
+}

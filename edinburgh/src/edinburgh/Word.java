65818bed2f816463aea6e265ec2dadc0c0f8679e
Giedriuszeb <Giedriuszeb@gmail.com>
2013-02-07 16:32:56 -0800

    Create Word.java

---
+++ edinburgh/src/edinburgh/Word.java
@@ -0,0 +1,25 @@
+public class Word{
+int count;
+String word;
+Word next;
+
+public Word (String newWord){
+count =0;
+word=newWord;
+next=null;
+}
+
+public void set(String str){
+count=0;
+word=str;
+}
+
+public Word (){
+count =0;
+word=null;
+next=null;
+}
+
+
+
+}
